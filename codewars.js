68fed45cc562c02f280a37b146a42ab49fff1872
Alesya Livanova <truealesya@gmail.com>
2022-01-21 02:13:41 +0300

    codewars

---
+++ codewars.js
@@ -0,0 +1,14 @@
+// 1
+multiply = function (a, b) {
+    return a * b;
+}
+
+// 2 Complete the solution so that it reverses the string passed into it.
+function solution(str){
+return str.split('').reverse().join('')
+}
+
+// 3 Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+function getChar(c){
+    return String.fromCharCode(c);
+}
