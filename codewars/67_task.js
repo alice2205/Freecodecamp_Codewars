6bb3a3aba5281dcdc717ed0a5303e8caa2249e39
Alesya Livanova <truealesya@gmail.com>
2022-07-18 18:38:06 +0300

    refactor

--- codewars/67_task.js
+++ codewars/67_task.js
@@ -1,6 +1,9 @@
 // Count of positives / sum of negatives
 function countPositivesSumNegatives(input) {
   let sum = [0, 0];
+  if (input == null || input.length < 1){
+  return [];
+  } 
   for (let i = 0; i<input.length; i++) {
     if (input[i]>0) {
       sum[0]+=1;
